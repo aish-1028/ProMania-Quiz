ef279a7a403f7cee6a856f02b7b694531d33cdf3
Aishwarya Jain <jainaish28@gmail.com>
2020-12-15 22:37:38 +0530

    quiz 1st commit

---
+++ src/quizService/index.js
@@ -0,0 +1,377 @@
+const qBank = [
+  {
+    question:
+      "Who invented C Language.?   ",
+    answers: ["Charles Babbage","Grahambel","Dennis Ritchie","Steve Jobs"],
+    correct: "Dennis Ritchie",
+    questionId: "099099"
+  },
+  {
+    question:
+      'C Language is a successor to which language.?',
+    answers: ["FORTRAN","D Language","BASIC","B Language"],
+    correct: "B Language",
+    questionId: "183452"
+  },
+  {
+    question:
+      "C is a which level language.?",
+    answers: ["Low Level","High Level","Low + High","None"],
+    correct: "High Level",
+    questionId: "267908"
+  },
+  {
+    question: "Low level language is .?",
+    answers: [
+      "Human readable like language",
+      "language with big program size",
+      "language with small program size",
+      "Difficult to understand and readability is questionable."
+    ],
+    correct: "Difficult to understand and readability is questionable.",
+    questionId: "333247"
+  },
+  {
+    question: "High level language is a .?s?",
+    answers: ["Human readable like language",
+    "language with big program size",
+    "language with small program size",
+    "Difficult to understand and readability is questionable."],
+    correct: "Human readable like language",
+    questionId: "496293"
+  },
+  {
+    question:
+      "C is _______ type of programming language.?",
+    answers: [
+      "Object Oriented",
+      "Procedural",
+      "Bit level language",
+      "Functional"
+    ],
+    correct: "Procedural",
+    questionId: "588909"
+  },
+  {
+    question:
+      'What is the present C Language Standard.?',
+    answers: ["C99 ISO/IEC 9899:1999","C11 ISO/IEC 9899:2011","C05 ISO/IEC 9899:2005","C10 ISO/IEC 9899:2010"],
+    correct: "C11 ISO/IEC 9899:2011",
+    questionId: "648452"
+  },
+  {
+    question: "C language was invented in which laboratories.?",
+    answers: ["Uniliver Labs","IBM Labs","AT&T Bell Labs","Verizon Labs"],
+    correct: "AT&T Bell Labs",
+    questionId: "786649"
+  },
+  {
+    question:
+      "BCPL Language is also called..?",
+    answers: ["C Language", "B Language", "D Language", "None"],
+    correct: "None",
+    questionId: "839754"
+  },
+  {
+    question:
+      "C language was invented to develop which Operating System.? ",
+    answers: [
+      "Android",
+      "Linux",
+      "Ubuntu",
+      "Unix"
+    ],
+    correct: "Unix",
+    questionId: "98390"
+  },
+  {
+    question: "C language was invented in the year.?",
+    answers: ["1962", "1982", "1972", "1990"],
+    correct: "1972",
+    questionId: "1071006"
+  },
+  {
+    question: "C language is used in the development of .?",
+    answers: ["Databases", "Graphic Applications", "Word Processors", "All of the above"],
+    correct: "All of the above",
+    questionId: "1174154"
+  },
+  {
+    question: "A C program is a combination of.?",
+    answers: ["Statements", "Functions", "Variables", "All of the above"],
+    correct: "All of the above",
+    questionId: "1226535"
+  },
+  {
+    question: "Single Line Comment // is also called.?",
+    answers: ["C++ Style Comment", "Java Style Comment", "PHP Style Comment", "All of the above"],
+    correct: "All of the above",
+    questionId: "1310938"
+  },
+  {
+    question: "What is the number of characters used to distinguish Identifier or Names of Functions and Global variables.?    ",
+    answers: ["31", "32", "33", "28"],
+    correct: "31",
+    questionId: "1436873"
+  },
+  {
+    question: "An Identifier can start with.?",
+    answers: ["Alphabet", "_Underscore", "Any Character", "Option A&B"],
+    correct: "Option A&B",
+    questionId: "1515110"
+  },
+  {
+    question: "Find an integer constant.",
+    answers: ["19", "20.45", '"12"', "None"],
+    correct: "19",
+    questionId: "1642728"
+  },
+  {
+    question:"Find a Floating Point constant.",
+    answers: ["12.3E5","12e34","125.34857","All the above"],
+    correct: "All the above",
+    questionId: "1747256"
+  },
+  {
+    question:
+      "Number of Keywords present in C Language are .?",
+    answers: ["32", "34", "62", "64"],
+    correct: "32",
+    questionId: "1822532"
+  },
+  {
+    question: "Each statement in a C program should end with.?",
+    answers: [
+      "Period . ",
+      "Colon : ",
+      "SemiColon ; ",
+      "None"
+    ],
+    correct: "SemiColon ; ",
+    questionId: "195075"
+  },
+  {
+    question: 'Find a correct C Keyword below.',
+    answers: ["breaker", "go to", "shorter", "default"],
+    correct: "default",
+    questionId: "2019778"
+  },
+  {
+    question: "Types of Real numbers in C are.?    ",
+    answers: ["float", "double", "long double", "All the above"],
+    correct: "All the above",
+    questionId: "2134343"
+  },
+  {
+    question: "Size of a Turbo C C++ compiler is.?",
+    answers: [
+      "16 Bit",
+      "32 Bit",
+      "64 Bit",
+      "128 Bit"
+    ],
+    correct: "16 Bit",
+    questionId: "2210799"
+  },
+  {
+    question:
+      "Sizes of short, int and long in a Turbo C C++ compiler in bytes are.?      ",
+    answers: ["2,2,4", "2,4,4", "4,8,16", "8,8,16"],
+    correct: "2,2,4",
+    questionId: "2379831"
+  },
+  {
+    question: "Find a C Storage Class below.",
+    answers: ["static", "auto", "register & extern", "All the above"],
+    correct: "All the above",
+    questionId: "2426418"
+  },
+  {
+    question: "What is the default C Storage Class for a variable.?",
+    answers: ["static", "auto", "register", "extern"],
+    correct: "auto",
+    questionId: "2510086"
+  },
+  {
+    question: "A register variable is stored in a Register. Where does a Register Present in a Computer.?",
+    answers: [
+      "RAM",
+      "ROM",
+      "CPU",
+      "DMA"
+    ],
+    correct: "CPU",
+    questionId: "2685745"
+  },
+  {
+    question:
+      "Variables of type auto, static and extern are all stored in .?",
+    answers: ["ROM", "RAM", "CPU", "DMA"],
+    correct: "RAM",
+    questionId: "2796884"
+  },
+  {
+    question: "Which among the following is a Local Variable.?",
+    answers: ["register", "auto", "static", "extern"],
+    correct: "auto",
+    questionId: "2838900"
+  },
+  {
+    question: 'Operator % in C Language is called.?    ',
+    answers: ["Percentage Operator", "Quotient Operator", "Modulus", "Division"],
+    correct: "Modulus",
+    questionId: "298060"
+  },
+  {
+    question: "Choose a C Conditional Operator from the list.",
+    answers: ["?:", ":?", ":<", "<:"],
+    correct: "?:",
+    questionId: "3096579"
+  },
+  {
+    question: "What is the other name for C Language ?: Question Mark Colon Operator.?    ",
+    answers: [
+      "Comparison Operator",
+      "If-Else Operator",
+      "Ternary Operator",
+      "Binary operator"
+    ],
+    correct: "Ternary Operator",
+    questionId: "3182461"
+  },
+  {
+    question: "Loops in C Language are implemented using.?",
+    answers: [
+      "While Block",
+      "For Block",
+      "do While Block",
+      "All"
+    ],
+    correct: "All",
+    questionId: "3239112"
+  },
+  {
+    question: "Which loop is faster in C Language, for, while or Do While.?    ",
+    answers: ["while", "for", "do while", "All work at same speed"],
+    correct: "All work at same speed",
+    questionId: "3318503"
+  },
+  {
+    question:
+      'What is the way to suddenly come out of or Quit any Loop in C Language.?',
+    answers: ["continue;", "break;", "leave;", "quit;"],
+    correct: "break;",
+    questionId: "3410327"
+  },
+  {
+    question: "A function which calls itself is called a ___ function.",
+    answers: ["Self Function", "Auto Function", "Recursive Function", "Static Function"],
+    correct: "Recursive Function",
+    questionId: "3542596"
+  },
+  {
+    question: "What are types of Functions in C Language.?",
+    answers: ["Library Function", "User Defined Function", "Both", "None"],
+    correct: "Both",
+    questionId: "3667517"
+  },
+  {
+    question: "Every C Program should contain which function.?",
+    answers: [
+      "printf()",
+      "getch()",
+      "scanf()",
+      "main()"
+    ],
+    correct: "main()",
+    questionId: "3791672"
+  },
+  {
+    question: "What is the minimum number of functions to be present in a C Program.?",
+    answers: ["1", "2", "3", "4"],
+    correct: "1",
+    questionId: "3893585"
+  },
+  {
+    question: 'Arguments passed to a function in C language are called ___ arguments.',
+    answers: ["Formal Arguments", "Actual Arguments", "Definite Arguments", "Ideal Arguments"],
+    correct: "Actual Arguments",
+    questionId: "3913430"
+  },
+  {
+    question: "Arguments received by a function in C language are called ___ arguments.    ",
+    answers: ["Formal Arguments", "Actual Arguments", "Definite Arguments", "Ideal Arguments"],
+    correct: "Formal Arguments",
+    questionId: "4049121"
+  },
+  {
+    question:
+      "Choose a non Library C function below.",
+    answers: ["printf()", "scanf()", "fprintf()", "fcprint()"],
+    correct: "fcprint()",
+    questionId: "4150746"
+  },
+  {
+    question:
+      "What is the default return value of a C function if not specified explicitly.?",
+    answers: ["-1", "0", "1", "None"],
+    correct: "1",
+    questionId: "4235063"
+  },
+  {
+    question: "A recursive function can be replaced with __ in c language.",
+    answers: ["for", "while", "do while", "All"],
+    correct: "All",
+    questionId: "4321002"
+  },
+  {
+    question: "A recursive function without If and Else conditions will always lead to.?",
+    answers: [
+      "Finite Loop",
+      "Infinite Loop",
+      "Incorrect result",
+      "Correct result"
+    ],
+    correct: "Infinite Loop",
+    questionId: "4442286"
+  },
+  {
+    question: "What is the C keyword that must be used to achieve expected result using Recursion.?    ",
+    answers: [
+      "printf",
+      "scanf",
+      "return",
+      "void"
+    ],
+    correct: "return",
+    questionId: "4564430"
+  },
+  {
+    question: "An array Index starts with.?",
+    answers: ["1", "0", "-1", "2"],
+    correct: "0",
+    questionId: "4630606"
+  },
+  {
+    question:
+      "If an integer array pointer is incremented, how many bytes will be skipped to reach next element location.?",
+    answers: ["1", "2", "8", "None"],
+    correct: "2",
+    questionId: "4795960"
+  },
+  {
+    question: "An entire array is always passed by ___ to a called function.",
+    answers: ["Call by Value", "Call by Reference", "Address Relocation", "Address restructure"],
+    correct: "Call by Reference",
+    questionId: "4811162"
+  },
+  {
+    question: "What is the function used to allocate memory to an array at run time with Zero initial value to each.?",
+    answers: ["calloc()", "malloc()", "palloc()", "kalloc()"],
+    correct: "calloc()",
+    questionId: "4982981"
+  }
+];
+
+export default (n = 5) =>
+  Promise.resolve(qBank.sort(() => 0.5 - Math.random()).slice(0, n));
